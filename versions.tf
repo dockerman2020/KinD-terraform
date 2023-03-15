4607c5486ecc6ef25b0a5d74e6132668ace8ca3f
Emmanuel Mamudu <emmanuel.mamudu@digital.homeoffice.gov.uk>
2023-03-15 10:37:35 +0000

    Initial commit.

---
+++ versions.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    kind = {
+      source  = "tehcyx/kind"
+      version = "0.0.16"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.18.1"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.9.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
