3242d495328b88e02056f06cfe1edb0e95f54b4a
Emmanuel Mamudu <emmanuel.mamudu@digital.homeoffice.gov.uk>
2023-03-17 17:08:28 +0000

    Cluster update

--- kind_cluster.tf
+++ kind_cluster.tf
@@ -6,38 +6,48 @@ provider "kubernetes" {
 }
 
 resource "kind_cluster" "default" {
-  name            = var.kind_cluster_name
-  kubeconfig_path = pathexpand(var.kind_cluster_config_path)
-  wait_for_ready  = true
+    name            = var.kind_cluster_name
+    node_image      = "kindest/node:v1.26.2"
+    kubeconfig_path = pathexpand(var.kind_cluster_config_path)
+    wait_for_ready  = true
 
   kind_config {
-    kind        = "Cluster"
-    api_version = "kind.x-k8s.io/v1alpha4"
-
-    node {
-      role = "control-plane"
-
-      kubeadm_config_patches = [
-        "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
-      ]
-      extra_port_mappings {
-        container_port = 80
-        host_port      = 80
+      kind        = "Cluster"
+      api_version = "kind.x-k8s.io/v1alpha4"
+      networking {
+        api_server_address = "192.168.68.120"
+        api_server_port   = 58366
       }
-      extra_port_mappings {
-        container_port = 443
-        host_port      = 443
-      }
-    }
+      containerd_config_patches = [
+        <<-TOML
+        [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:5000"]
+            endpoint = ["http://kind-registry:5000"]
+        TOML
+         ]
+      node {
+          role = "control-plane"
 
-    node {
-      role = "worker"
-    }
-    node {
-      role = "worker"
-    }
-    node {
-      role = "worker"
-    }        
+          kubeadm_config_patches = [
+            "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
+          ]
+
+          extra_port_mappings {
+              container_port = 80
+              host_port      = 80
+          }
+          extra_port_mappings {
+              container_port = 443
+              host_port      = 443
+          }
+      }
+      node {
+          role = "worker"
+      }
+      node {
+          role = "worker"
+      }
+      node {
+          role = "worker"
+      }
   }
 }
